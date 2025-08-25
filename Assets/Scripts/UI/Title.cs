1f9e861d1c0c8f40ecb10b70f201f45f5ff34aba
ㅇㅅㅇ <jhy041020@handong.ac.kr>
2025-08-25 23:47:40 +0900

    Update Title.cs

--- Assets/Scripts/UI/Title.cs
+++ Assets/Scripts/UI/Title.cs
@@ -1,18 +1,49 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Burst.Intrinsics;
 using UnityEngine;
+using UnityEngine.UIElements;
 
-public class Title : MonoBehaviour
+public class Title : MonoBehaviour, IUIState
 {
-    // Start is called before the first frame update
-    void Start()
+    private UIDocument uI;
+    List<Button> btns = new List<Button>();
+    VisualElement popUp;
+    public void Enter()
     {
-        
+        gameObject.SetActive(true);
+        uI = gameObject.GetComponent<UIDocument>();
+        var root = uI.rootVisualElement;
+        btns.AddRange(root.Q<VisualElement>().Query<Button>().ToList());
+        popUp = root.Q<VisualElement>("Overlay");
+        btns.Add(popUp.Q<VisualElement>().Q<Button>());
+        UIManager.instance.Hide(popUp);
+        foreach (var btn in btns)
+        {
+            btn.clicked += () => OnClick(btn.name);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    void OnClick(string btnName)
     {
-        
+        switch (btnName)
+        {
+            case "PlayBtn":
+                UIManager.instance.SetState(UIManager.State.InGame);
+                break;
+            case "HelpBtn":
+                UIManager.instance.Show(popUp);
+                break;
+            case "BackBtn":
+                UIManager.instance.Hide(popUp);
+                break;
+            case "QuitBtn":
+                GameManager.instance.QuitGame();
+                break;
+        }
+    }
+    public void Exit()
+    {
+        gameObject.SetActive(false);
     }
 }
