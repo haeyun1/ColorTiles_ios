16c028ebd3d841a73b727d7126920eacd87ea5e5
ㅇㅅㅇ <jhy041020@handong.ac.kr>
2025-08-26 00:43:18 +0900

    Update InGame.cs

--- Assets/Scripts/UI/InGame.cs
+++ Assets/Scripts/UI/InGame.cs
@@ -5,6 +5,42 @@ using UnityEngine.UIElements;
 
 public class InGame : MonoBehaviour, IUIState
 {
-    public void Enter(){}
-    public void Exit(){}
+    private UIDocument uI;
+    Button homeBtn;
+    ProgressBar time;
+    Label score;
+
+    float maxTime;
+    public void Enter()
+    {
+        gameObject.SetActive(true);
+        uI = GetComponent<UIDocument>();
+
+        var root = uI.rootVisualElement;
+        homeBtn = root.Q<Button>();
+        homeBtn.clicked += OnClick;
+        time = root.Q<ProgressBar>();
+        score = root.Q<Label>("Score");
+
+        GameManager.instance.ResetScore();
+        maxTime = GameManager.instance.GetMaxTime();
+        GameManager.instance.StartTimer();
+        UIManager.instance.ShowTile(true);
+    }
+    void OnClick()
+    {
+        UIManager.instance.SetState(UIManager.State.Title);
+    }
+    void Update()
+    {
+        score.text = GameManager.instance.GetScore();
+        time.value = GameManager.instance.GetTime() / maxTime;
+    }
+
+    public void Exit()
+    {
+        gameObject.SetActive(false);
+        UIManager.instance.ShowTile(false);
+        GameManager.instance.StopTimer();
+    }
 }
