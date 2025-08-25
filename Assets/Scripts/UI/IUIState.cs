c43df38db3c8d95a92c35ba3b03542e65f50b403
ㅇㅅㅇ <jhy041020@handong.ac.kr>
2025-08-25 23:47:26 +0900

    인터페이스 추가

---
+++ Assets/Scripts/UI/IUIState.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public interface IUIState
+{
+    void Enter();
+    void Exit();
+}
+
