42340d280807245982c8017b39d34b1bf5e11ee0
ㅇㅅㅇ <jhy041020@handong.ac.kr>
2025-08-25 23:47:36 +0900

    Update End.cs

--- Assets/Scripts/UI/End.cs
+++ Assets/Scripts/UI/End.cs
@@ -1,18 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
-public class End : MonoBehaviour
+public class End : MonoBehaviour, IUIState
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public void Enter(){}
+    public void Exit(){}
 }
